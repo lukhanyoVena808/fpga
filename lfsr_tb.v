47eca3400d52b3dec6586c04b5c692641598cbe5
BinaM23 <100226104+BinaM23@users.noreply.github.com>
2023-05-21 21:11:36 +0200

    Update lfsr_tb.v

--- lfsr_tb.v
+++ lfsr_tb.v
@@ -25,39 +25,21 @@ module lfsr_tb(
     );
     localparam N = 32;
     parameter Num=3;
-   // reg [Num:1]clk, reset_n;
-   // wire [Num:1] Q;
-    
     reg clk,reset_n;
-    //wire Q;
     wire [1:N] Q;
     
 
-    
+    //instnatiate the module that makes multiple LFSRs
     RNG #(.Num(Num)) md(.clk(clk),.reset_n(reset_n),.Q(Q));
    
     
-    // Instantuate unit under test
+    // Instantuate unit under test, to test 1 LFSR alone
    /* lfsr #(.N(N),.seed(2)) uut (
         .clk(clk),
         .reset_n(reset_n),
         .Q(Q)
     );*/
   
-   
-    
-   /* generate
-        genvar i;
-        for (i = 1; i < NUM_LFSR; i = i + 1) begin : INSTANCES
-            lfsr #(.N(N),.seed(seedArray[i)) uut (
-                .clk(clk),
-                .reset_n(reset_n),
-                .Q(Q)
-            );
-        end
-    endgenerate*/
-    
-   
     // Generating a clk signal
     localparam T = 10; //10ns
     always
@@ -67,39 +49,23 @@ module lfsr_tb(
         clk = 1'b1;
         #(T / 2);
     end
-    // generator #(.Num(Num)) MOD(.seedArr(seedArray))
-    /*
-    initial begin
-        clk = 1'b0;
-        forever begin
-            #(5);
-            clk = ~clk;
-        end
-    end
     
-    
-    */
     
     initial
     begin
         // issue a quick reset for 2 ns
         reset_n = 1'b0;
-        #2 //#5; 
+        #2 
         reset_n = 1'b1;
        
         $monitor("Q:%d",Q);
         
-        
-      
-        
-   
         repeat(2) @(negedge clk);
         wait (Q == 1); //halts exec until Q=1 then stops
         //stops simulation when a full period has elapsed if we start at 1
         //generates a random number every 10ns
         //runs for 1000ns so generates 100 random numbers sequentially
-        
-        //#20 $stop;
+  
        #100 $finish;
     end
     
