c88536510761801fb65ef858f1fefecd9c41e60c
BinaM23 <100226104+BinaM23@users.noreply.github.com>
2023-05-21 21:13:40 +0200

    Update lfsr.v

--- lfsr.v
+++ lfsr.v
@@ -23,15 +23,10 @@
 module lfsr
     #(parameter N = 32,parameter seed ='d1)( //No of bits =N, can specify seed at instantiation
     input clk,
-    //input seed,
     input reset_n,
     output [1:N] Q//generated random number register
-    
-    
     );
     
-    
-   // reg [32:1]count = 32'b0;
     //Assumes MSB is bit 1, LSB=bit N, not strating from 0 but this can be changed
     reg [1:N] Q_reg, Q_next;
     wire taps; //positions that are XOR to get next state
@@ -39,7 +34,7 @@ module lfsr
     always @(posedge clk, negedge reset_n)//on positive clk or negative reset
     begin
         if (~reset_n) //if reset==0, initial state
-            //Q_reg <= 'd1; // LSFR seed can be anything except 0 or FFFFFFFF
+ // LSFR seed can be anything except 0 or FFFFFFFF
             Q_reg <= seed;
         else
             Q_reg <= Q_next; //assign the next generated number to register
@@ -66,12 +61,9 @@ endmodule
 
 module RNG
  #(parameter Num=3)(
- //input [Num:1] clk,
-// input [Num:1] reset_n,
  input clk,
  input reset_n,
  output [1:32] Q
- //output [Num:1] Q  // [Num-1:0] Q [1:N]
  
  );
  
@@ -80,14 +72,7 @@ module RNG
  lfsr #(.N(32),.seed(2)) LFSR2(clk,reset_n,Q1);
  lfsr #(.N(32),.seed(3)) LFSR3(clk,reset_n,Q2);
  lfsr #(.N(32),.seed(4)) LFSR4(clk,reset_n,Q3);
- 
+ //only way to generate multiple LFSRs
  assign Q={Q1,Q2,Q3};
  
- /*genvar i;
- generate 
-    for(i=0;i<Num;i=i+1)begin
-        lfsr #(.N(32)) LFSR (clk[i],reset_n[i],Q[i]);
-        end
- endgenerate 
- */
 endmodule 
